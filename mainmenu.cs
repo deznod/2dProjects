276dbe6ec49f7acca457742c031e71d4fc76a2e3
deznod <fredomworld1@gmail.com>
2018-06-30 04:40:09 +0300

    Илья
    Добавлен скрипт для сцены с главным меню
    
    Co-Authored-By: BatonGamedesigner <batongamedesigner@users.noreply.github.com>

---
+++ mainmenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class mainmenu : MonoBehaviour {
+
+    public void PlayPressed()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitPressed()
+    {
+        Application.Quit();
+    }
+}
