5b3713eebe8ffa9fb0115443f04412791164059d
deznod <fredomworld1@gmail.com>
2018-06-30 03:48:56 +0300

    Илья
    Создана анимация и привязанная камера к игроку
    
    Co-Authored-By: BatonGamedesigner <batongamedesigner@users.noreply.github.com>

---
+++ CameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	// Хранит ссылку на объект Player
+	public GameObject player;
+
+	// Хранит растояние до камеры от инрока в любое время
+	private Vector3 offset;
+    
+	void Start ()
+	{
+		// Создает смещение вычитая положение камеры из позиции игрока
+		offset = transform.position - player.transform.position;
+	}
+
+	void LateUpdate ()
+	{
+		// Устанавливает положение камеры 
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	void Update () 
+	{
+        // Поварачивает объект на 15 по X, 30 по Y, и 45 по Z, 
+        //умноженной на deltaTime, чтобы сделать ее в секунду а не на кадр
+        transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+	}
+}	
